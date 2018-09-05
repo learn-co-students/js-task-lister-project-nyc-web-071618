c2c6a466c4d4ff001bd4f4cfa7b30eca6039e787
chejianchao <820992376@qq.com>
2018-09-05 16:40:37 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // grab DOM elements
   const listDiv = document.getElementById("app-content");
 
+
   const app = new TaskLister();
+
+
+  listDiv.addEventListener("click", e => {
+    if (e.target.className === "delete-list") {
+      app.deleteList(e.target.dataset.title);
+      app.render()
+    } else if (e.target.className === "delete-task") {
+      app.deleteTask(e.target.dataset.listTitle, e.target.dataset.taskName);
+      app.render()
+    }
+  });
+
 });
--- src/taskLister.js
+++ src/taskLister.js
@@ -1,6 +1,70 @@
 class TaskLister {
   // your solution here
+  constructor(){
+    this.taskList = []
+    const createListForm = document.getElementById("create-list-form")
+    createListForm.addEventListener("submit", (e)=>{
+      e.preventDefault()
+
+      let name = document.getElementById("new-list-title")
+      this.createNewList(name.value)
+      this.render()
+      name.value = ""
+    })
+
+    const createTaskForm = document.getElementById("create-task-form")
+    createTaskForm.addEventListener("submit", (e)=>{
+      e.preventDefault()
+      const parentList = document.getElementById("parent-list")
+      const description = document.getElementById("new-task-description")
+      const priority   = document.getElementById("new-task-priority")
+      const listName= parentList[parentList.selectedIndex].value;
+      const priorityValue = priority.value
+      if( priorityValue === "" )
+        priorityValue = "low"
+      //const task = new Task(listName, description.value, priority.value)
+
+      const list = this.taskList.find(list=>list.name === listName)
+      list.createNewTask(description.value, priorityValue)
+      this.render()
+    })
+  }
+
+  createNewList(name){
+    let list = new List(name)
+    this.taskList.push(list)
+  }
+
+  deleteList(listTitle) {
+    console.log("deleteList ", listTitle)
+    this.taskList = this.taskList.filter(({ name }) => listTitle !== name);
+  }
+
+  deleteTask(listTitle, taskDescription) {
+    console.log("delete task  ", listTitle, taskDescription)
+    const targetList = this.taskList.find(({ name }) => name === listTitle);
+    targetList.deleteTask(taskDescription);
+  }
+
   render() {
-    return (`<h1>Welcome to Flavortown</h1>`);
+    const lists = document.getElementById("lists")
+    lists.innerHTML = ""
+    const parentList = document.getElementById("parent-list")
+    parentList.innerHTML = ""
+    for( let i = 0; i<this.taskList.length; i++){
+      const div = document.createElement("div")
+      div.innerHTML = this.taskList[i].render()
+      lists.appendChild(div)
+      let listName = this.taskList[i].name
+      parentList.innerHTML += `<option value="${listName}">
+        ${listName}</option>`
+    }
+    const createTaskForm = document.getElementById("create-task-form")
+
+    if( this.taskList.length > 0 ){
+      createTaskForm.style.display = "block"
+    }else {
+      createTaskForm.style.display = "none"
+    }
   }
 }
